e3924336848a1226a686ca0c6e8455afd8b120f5
Arthur Petrie <contact@arthurpetrie.com>
2020-04-14 15:16:31 +0200

    update deps

--- packages/macro/test/macro.test.js
+++ packages/macro/test/macro.test.js
@@ -11,7 +11,7 @@ pluginTester.default({
     "no usage": `import tw from '../lib/macro'`,
     "correct usage 1": `
       import tw from '../lib/macro';
-      const css = tw('w-1/2 bg-blue-500 scale-0 active:bg-red-300 lg:hover[text-red-200 bg-blue-500]', 'lg:text-red-300',"focus:bg-green-300", "md:bg-green-300");
+      const css = tw('w-1/2 bg-blue-500 scale-0 active:bg-red-300 lg:hover[text-red-200 bg-blue-500]', ['lg:text-red-300',"focus:bg-green-300"], "md:bg-green-300");
     `,
     "correct usage 2": `
       import tw from '../lib/macro';
