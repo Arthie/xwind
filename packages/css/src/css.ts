667df98e684a61413026bf540ed5ede21e9fe6f1
Arthur Petrie <contact@arthurpetrie.com>
2020-10-19 01:54:25 +0200

    add css package

---
+++ packages/css/src/css.ts
@@ -0,0 +1,7 @@
+import clsx from "clsx";
+
+export default (classes: TemplateStringsArray): string => {
+  throw new Error("@tailwindcssinjs - Configure the babel plugin correctly");
+};
+
+export const cx = clsx;
