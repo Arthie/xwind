fe9cf3916044c68bff30caa9f1bc98b238d03fd0
Arthur Petrie <contact@arthurpetrie.com>
2020-11-21 20:54:41 +0100

    update xwind tests

---
+++ packages/xwind/test/example.test.ts
@@ -0,0 +1,41 @@
+import pluginTester from "babel-plugin-tester";
+
+import plugin from "../lib/babel";
+
+import core, { createTwClassDictionary, resolveConfig } from "@xwind/core";
+
+const tests = () => {
+  const resolvedConfig = resolveConfig(
+    require("../../../config/tailwindconfigs/example")
+  );
+  const { utilitiesRoot, componentsRoot } = core(resolvedConfig);
+  const twObjectMap = createTwClassDictionary(utilitiesRoot, componentsRoot);
+
+  let tests = [];
+  for (const key of Object.keys(twObjectMap)) {
+    tests.push([
+      key,
+      {
+        code: `
+      import xw from 'xwind';
+      const css = xw\`${key}\`;
+      `,
+        snapshot: true,
+      },
+    ]);
+  }
+  return Object.fromEntries(tests);
+};
+
+pluginTester({
+  plugin,
+  pluginName: "xwind",
+  snapshot: true,
+  babelOptions: {
+    filename: __filename,
+  },
+  pluginOptions: {
+    config: "./config/tailwindconfigs/example.js",
+  },
+  tests: tests(),
+});
