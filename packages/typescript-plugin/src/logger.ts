75cf661cf7103c9ba52643ff2f7ef5651e90edf1
Arthur Petrie <contact@arthurpetrie.com>
2020-11-24 10:28:47 +0100

    prettier logger

--- packages/typescript-plugin/src/logger.ts
+++ packages/typescript-plugin/src/logger.ts
@@ -3,9 +3,7 @@ import { Logger } from "typescript-template-language-service-decorator";
 export function createLogger(info: ts.server.PluginCreateInfo): Logger {
   return {
     log(msg: string): void {
-      info.project.projectService.logger.info(
-        `>> [XWINDTSPLUGIN] ${msg}`
-      );
+      info.project.projectService.logger.info(`>> [XWINDTSPLUGIN] ${msg}`);
     },
   };
 }
